dc21df41e9ceee65f241c543b4b85c9780ced67e
Soban Syeed <Soban.syeed@outlook.com>
2025-01-11 19:43:41 +0530

    Complete - Time.deltaTime

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float moveSpeed = 10f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
@@ -11,9 +13,9 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xValue = Input.GetAxis("Horizontal");
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float yValue = 0f;
-        float zValue = Input.GetAxis("Vertical");
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
 
         transform.Translate(xValue, yValue, zValue);
     }
