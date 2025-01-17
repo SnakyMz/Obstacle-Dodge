1cb036f13fd5a157986dd1df86793ea5bf37cd0a
Soban Syeed <Soban.syeed@outlook.com>
2025-01-17 23:44:41 +0530

    Color change using GetComponent

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -4,6 +4,7 @@ public class ObjectHit : MonoBehaviour
 {
     void OnCollisionEnter(Collision other)
     {
+        GetComponent<MeshRenderer>().material.color = Color.black;
         Debug.Log("Something hit me");
     }
 }
