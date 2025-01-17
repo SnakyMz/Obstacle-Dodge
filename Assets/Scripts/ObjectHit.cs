c87dba69b48e9b4fba1ba0962509d688b7753806
Soban Syeed <Soban.syeed@outlook.com>
2025-01-17 23:32:40 +0530

    Update ObjectHit

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -2,15 +2,8 @@ using UnityEngine;
 
 public class ObjectHit : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void OnCollisionEnter(Collision other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Something hit me");
     }
 }
