17669f6c05d2d3e8531d477ee105232b64ee661e
Soban Syeed <Soban.syeed@outlook.com>
2025-01-21 22:53:54 +0530

    Add FlyToPlayer functionality

--- Assets/Scripts/FlyToPlayer.cs
+++ Assets/Scripts/FlyToPlayer.cs
@@ -1,16 +1,21 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FlyToPlayer : MonoBehaviour
 {
+    [SerializeField] float speed = 1f;
+    [SerializeField] Transform player;
+    Vector3 playerPosition;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerPosition = player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, playerPosition, speed * Time.deltaTime);
     }
 }
